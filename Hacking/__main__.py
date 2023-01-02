305d3a2c3a1fb0e1b86f0c6c6001fe82d2c60aeb
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-01-02 22:55:38 +0300

    Update and rename __main__.py to main.py

--- Hacking/__main__.py
+++
@@ -1,9 +0,0 @@
-from Hacking import hack
-from pyrogram import idle
-from Hacking import app, LOGGER 
-from config import * 
-
-LOGGER.info("Bot çalıştı... Hileci herif seni 😂😂😂") 
-
-app.run()
-idle()
---
+++ Hacking/main.py
@@ -0,0 +1,16 @@
+from Hacking import app
+from pyrogram import idle
+from pyrogram import Client
+from config import API_ID, API_HASH, SESSION_NAME
+
+
+app = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    session_name=SESSION_NAME,
+    plugins=dict(root="Hacking")
+)
+
+app.run()
+idle() 
