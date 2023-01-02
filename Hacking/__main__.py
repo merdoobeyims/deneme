e5bb086760648808fe195963b4da90b2aca61c73
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-01-02 22:28:01 +0300

    Update __main__.py

--- Hacking/__main__.py
+++ Hacking/__main__.py
@@ -1,11 +1,7 @@
-import logging
-from pyrogram import Client, idle
-from config import API_HASH, API_ID, SESSION_NAME 
+from pyrogram import Client, idle 
 from Hacking.modules import *
 from Hacking import app, LOGGER
  
-
-
 
 app.start()
 LOGGER.info("Hadi iyisin hileci herif seni 😂😂😂")
