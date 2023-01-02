0b2b90df6eb1f81a77199f7bfcdcbb4128be0a2c
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-01-02 21:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from Hacking import 
-from pyrogram import idle, filters
-from pyrogram.types import InlineKeyboardMarkup
-from pyrogram.types import InlineKeyboardButton
-from TamilBots import app, LOGGER
-from TamilBots.TamilBots import ignore_blacklisted_users
-from TamilBots.sql.chat_sql import add_chat_to_db
-
-
-
 import logging
 from pyrogram import Client, idle
 from config import API_HASH, API_ID, SESSION_NAME 
+from Hacking import timedcapture
+from Hacking import app, LOGGER
+ 
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -23,3 +16,4 @@ app = Client("Timed", api_hash=API_HASH, api_id=API_ID, session_name=SESSION_NAM
 app.start()
 LOGGER.info("Hadi iyisin hileci herif seni 😂😂😂")
 idle()
+
