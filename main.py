1e1f703e740578e2ccfa5752a65d6285a92783c7
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-01-02 23:38:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait, MessageNotModified, MessageIdInvalid
 from time import sleep, strftime, gmtime, time
 from os.path import join
 from random import randint
-from config import *
+import config 
 
 app = Client(config.SESSION_NAME, config.API_ID, config.API_HASH) 
 
