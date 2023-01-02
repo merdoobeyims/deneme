0c7235e04a6ad56bd74c247e41ed98abf81e150d
Gecelerin Lordu <121420310+LordKing06@users.noreply.github.com>
2023-01-02 22:26:48 +0300

    Update __init__.py

--- Hacking/__init__.py
+++ Hacking/__init__.py
@@ -1,1 +1,10 @@
 # Ne olmasını bekliyordun ki :) 
+import logging 
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+
+app = Client("Timed", api_hash=API_HASH, api_id=API_ID, session_name=SESSION_NAME)
