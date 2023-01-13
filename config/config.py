6f393ed0742f4739847e27eb948459d13a1a4ac7
Veysitasci <veysi7074@gmail.com>
2023-01-13 21:43:43 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,5 +9,5 @@ admins = {}
 SESSION_NAME = getenv("session_name", "BABpmu72j1duLmfXxBFay_wv0ZCPRyYibyzhvrUSRteOdmj6KU9hZunlIbrijMflW30U4VshD9lYKHM0yDrJgTXNv-FHUH94DIy6UWHWVI5NynpaPh5KHAr57KjglyRJAsod8QxZ9LjZoLC05tSkjcKtjhYNq5QW4hzHVxl6bj7xXXOg7OTjkywgqNgUtzPbwhFg_SAAJwV4q6Ib5ydyMh7b204AUiseF0mKotv5-gc_5gRj0uQY928Na0ZiLzRdBHIZtFJLZ7Apo3-D7YfAEnW4SFyfYPinZHhF_RUhjemXFSXA6cNA4MjYZXlw_xjTcxbis4LNKa6Iouvqas4cvNsZUhAOHgA")
 API_ID = int(getenv("api_id", "9316256"))
 API_HASH = getenv("api_hash", "5a8a277605c3038129c536a9e79cd761")
-CHANNEL_ID = getenv("channel_id", "-1001853114058")
+CHANNEL_ID = getenv("channel_id", "-1001853845425")
 last_messages_amount = 50 
