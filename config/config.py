c0b46979276019dd217b6ddf489b5b7ea0320949
Veysitasci <veysi7074@gmail.com>
2023-01-15 07:17:09 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("session_name", "BABpmu72j1duLmfXxBFay_wv0ZCPRyYibyzhvrUSRteOdmj6KU9hZunlIbrijMflW30U4VshD9lYKHM0yDrJgTXNv-FHUH94DIy6UWHWVI5NynpaPh5KHAr57KjglyRJAsod8QxZ9LjZoLC05tSkjcKtjhYNq5QW4hzHVxl6bj7xXXOg7OTjkywgqNgUtzPbwhFg_SAAJwV4q6Ib5ydyMh7b204AUiseF0mKotv5-gc_5gRj0uQY928Na0ZiLzRdBHIZtFJLZ7Apo3-D7YfAEnW4SFyfYPinZHhF_RUhjemXFSXA6cNA4MjYZXlw_xjTcxbis4LNKa6Iouvqas4cvNsZUhAOHgA")
-API_ID = int(getenv("api_id", "9316256"))
-API_HASH = getenv("api_hash", "5a8a277605c3038129c536a9e79cd761")
-CHANNEL_ID = getenv("channel_id", "-1001853845425")
+SESSION_NAME = getenv("session_name", "BABokzZBgvllYFsv-hqUiFc-i5ym7IJ-LHd4uJnMbYdmJzcPBZ0JgqCgzvc7mSKH3fg6MQ3h5R3TBvVzYV2HdfYeqeqYpei-nx2yIKUFcWUa7rSwE9if76ddMC98HxdloGRPxo8j3Ctf0Pgd4nyVywTZHst9yjckVFkdTB0y9LDrRJHm9i-Nl2QpvtJEYVeU5hU5i6kjQ3YgWmVTi2nLfXUS1n_sfveeNH2H76oijIOgivi2TqqjxRmatP1xd2sp8gCrowD16WkDUIenjIslrta6R-_HAOCge5BbZJSNnwuJy5HqLKrGYG1doKOcdPWP_iY051eXM1SjeBPxgG_cFQlvAAAAAV9WO64A")
+API_ID = int(getenv("api_id", "20765181"))
+API_HASH = getenv("api_hash", "e8ec2b740ac91dfce31faa3ef654d1a4")
+CHANNEL_ID = getenv("channel_id", "-1001853114058")
 last_messages_amount = 50 
