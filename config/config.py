eebaa43d7c7f517aaab2991ae0816ee5f37e5f39
merdoobeyims <122889450+merdoobeyims@users.noreply.github.com>
2025-01-08 17:26:39 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("session_name", "AQHFv60AKe827yNmjHb5EIRoM4zUImFd9X-ldQNFc18YdKl5NVpSUWZ24pKeQvc6tX2KXath_s47r7-BQPz12o6wAmlbzGR_YK8odpnz70PGeo5OdBfeneGD8uV-K653ZxrD3O5QbHolQ-1AJQ0Tuo-dTjbEI7AEEoTVtJBGZakJtoV6flN-2BswCc65lVst3JCNMRuhjyTXUK96NRua9svQufe_0WIXAhBFdaMyE82mXFr_t7OokuEz7Vu-1ZG17LClBx_WB7v0QfgHnAtWV0LSRrJ8doEzweDgXuSKZkbn8GkB8zXjen289M6k9jOneg8JK_Vl6VuS_m-03n42IPEvvakRVQAAAAGO8SGEAA")
-API_ID = int(getenv("api_id", "29736877"))
-API_HASH = getenv("api_hash", "6238bdbf7f4a191a71e7e768dcfccc97")
-CHANNEL_ID = getenv("channel_id", "-1002350735231")
+SESSION_NAME = getenv("session_name", "BABVgLqVQc6nzVId22Yps0B69UMmsaeUdkOR-o8XXSA50YD_LKNNIVjsZttkzU7JuP-xejn5_aLQLN_tgopVB8wC3L5y0OTBbTnZnxxkcHh5v4ps7wpasopOrx9kkwfYYfYRd2JnraasozgfftgPSL9w2W7qzydhFxNT46J10uhgECDQQPOeqJqcicQCGRo7N0SLyiW4aAwVNxMV7SucUbzK7ZhwGfTlK0CkbRfTxS_YYQU0T2U0WFUm_IpUCsk2HrK_gO8j32J96trJkNHwPHmeRsDruE_s2lZd0xdWwlITYb_UbHNAadq3bWKsELWvSbAp6hTZgnHJgvXok1ta_DflAAAAAaxmpnYA")
+API_ID = int(getenv("api_id", "28113782"))
+API_HASH = getenv("api_hash", "72ac0911ddf53a020560e0dde09650a6")
+CHANNEL_ID = getenv("channel_id", "-1002311110965")
 last_messages_amount = 90
